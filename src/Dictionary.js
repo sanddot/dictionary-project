646f024d3ced63b9c73674aaaff3818ec483f894
sanddot <sanginta@gmail.com>
2024-12-06 14:47:52 -0800

    Added api call

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import "./dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    //documentation https://www.shecodes.io/learn/apis/dictionary/
+    let apiKey = "t3f3af9b184481d0306edc82cbo6ff8c";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function updateKeyword(event) {
