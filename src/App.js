5348d2da5921bcedba01d658b1d8869b37ec8262
sanddot <sanginta@gmail.com>
2024-12-13 16:58:06 -0800

    Updated footer

--- src/App.js
+++ src/App.js
@@ -16,6 +16,22 @@ export default function App() {
           <a href="https://github.com/sanddot" target="_blank" rel="noreferrer">
             Sandra Domeikiene
           </a>
+          , hosted on{" "}
+          <a
+            href="https://check-dictionary-project-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://github.com/sanddot/dictionary-project/commits?author=sanddot"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
         </footer>
       </div>
     </div>
